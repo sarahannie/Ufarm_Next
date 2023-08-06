5d8dce01e0af790746e9085303fc7d333db4c177
sarahannie <sarahndianekwu@gmail.com>
2023-08-06 21:18:10 +0100

    sidebar successful created

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,8 @@
+"use client"
 import './globals.css';
 import 'bootstrap/dist/css/bootstrap.css'
-import { Inter } from 'next/font/google'
+import { Inter } from 'next/font/google';
+import { useEffect } from 'react';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -10,6 +12,11 @@ export const metadata = {
 }
 
 export default function RootLayout({ children }) {
+
+  useEffect(()=>{
+    import("bootstrap/dist/js/bootstrap");
+  },[])
+
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
---
+++ page/_app.js
@@ -0,0 +1,7 @@
+
+import '../public/css/bootstrap/css/bootstrap.css'
+
+
+export default function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />
+}
