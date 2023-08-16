4a9de6d3bd5d58d2c5bac9a891241b335ef5fdcf
sarahannie <sarahndianekwu@gmail.com>
2023-08-16 11:11:09 +0100

    started header

--- app/page.js
+++ app/page.js
@@ -8,35 +8,15 @@ import Offer from '../component/offer'
 import Essential from '../component/essential'
 import Featured from '../component/featured'
 import FeaturedBrand from '../component/featuredBrand'
+import Banner from '../component/header'
+
 
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <Navbar />
-      <div>
-        {/* navbar start*/}
-        <div w3-include-html="../includes/navbar.html" />{/* navbar end*/}
-        {/* Carousel section start*/}
-        <div className="carousel slide" id="carouselExampleSlidesOnly" data-mdb-ride="carousel">
-          <div className="carousel-inner">
-            <div className="carousel-item active"><img className="d-block w-100" src="../images/carousel1.jpg" alt="Wild Landscape" />
-              <div className="carousel-content">
-                <p className="next">Next Level Technology</p>
-                <p className="Buy">Buy, Sell and Trade Products</p>
-                <p className="started">Get Started With Us</p><a className="btn btn-light" type="button" data-mdb-ripple-color="dark" href="/signup">Signup</a>
-              </div>
-            </div>
-            <div className="carousel-item"><img className="d-block w-100" src="../images/rec.png" alt="Camera" />
-              <div className="carousel-content">
-                <p className="next">Best Agricultural Products</p>
-                <p className="Buy">Let Go Green Again</p>
-                <p className="started">We Give Quality,Quantity <br /> and Best Products When It Comes To Agriculture</p><a className="btn btn-light" type="button" data-mdb-ripple-color="dark" href="/signup">Signup</a>
-              </div>
-            </div>
-          </div>
-        </div>{/* Carousel section end*/}
-      </div>
+      <Banner />
       <FeaturedBrand />
       <Featured />
       <Essential />
---
+++ component/header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Banner = () => {
+  return (
+    <div className={`containerfluid`}>
+        <div className={`container`}>
+            <div className={`grid`}>
+                    <h1>Hello</h1>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Banner
