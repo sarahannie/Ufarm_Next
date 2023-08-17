7555d9d563fabe83f641d5d6b2cc6e67f8a8667f
sarahannie <sarahndianekwu@gmail.com>
2023-08-17 16:19:53 +0100

    service done

--- app/service/page.jsx
+++ app/service/page.jsx
@@ -1,23 +1,13 @@
 import Navbar from '../../component/navBar';
 import Footer from '../../component/footer';
-import '../../public/css/service.css';
 import CardService from '../../component/cardService';
+import Search from '../../component/search';
 const page = () => {
   return (
     <div>
         <Navbar />
              {/* Searchbar start */}
-        <div className="container">
-          <div className="row height d-flex justify-content-center align-items-center">
-            <div className="col-md-8">
-              <div className="search">
-                <i className="fa fa-search" />
-                <input type="text" className="form-control" placeholder="Search For Products" />
-                <button className="btn btn-primary">Search</button>
-              </div>
-            </div>
-          </div>
-        </div>
+        <Search />
         {/* Searchbar End */}
         {/* Card start */}
         <div className="bg1">
--- component/search.jsx
+++ component/search.jsx
@@ -1,10 +1,21 @@
-import React from 'react'
+import React from 'react';
+import style from './search.module.css';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
 
 const Search = () => {
   return (
-    <div>
-        
+    <div className={`container ${style.container}`}>
+    <div className={`row height d-flex justify-content-center align-items-center ${style.height}`}>
+      <div className="col-md-8">
+        <div className={style.search}>
+          {/* <FontAwesomeIcon style={{fontSize:'30px'}} icon={faSearch} className={style.faSearch} /> */}
+          <input type="text" className={`form-control`} placeholder="Search For Products" />
+          <button className="btn btn-primary">Search</button>
+        </div>
+      </div>
     </div>
+  </div>
   )
 }
 
